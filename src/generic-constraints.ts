a0db5141ddf252d639bff6a82914233abf91bdcf
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-06-18 18:40:49 +0600

    constraints in generic

---
+++ src/generic-constraints.ts
@@ -0,0 +1,51 @@
+
+// Array Function
+
+const createArray = (param:string): string[] => {
+return [param]
+}
+
+//Generic for Arrow Function
+const createArray1 = <X,Y>(param1:X,param2:Y): [X,Y] => {
+return [param1,param2]
+}
+
+//Generic for Normal Function
+function createArray2 <X,Y>(param1:X,param2:Y): [X,Y] {
+return [param1,param2]
+}
+
+const result1 = createArray1<string, string>('Bangladesh','I Love my Country')
+const result2 = createArray1<boolean, string[]>(true,['USA'])
+
+type Name = {name:string}
+const result3 = createArray1<Name, boolean>({name: 'Bangladesh'}, true)
+
+
+//Spread Operator
+
+// const newData = {...myInfo,crush}
+
+
+type MendatoryType = {name: string, age: number, salary: number}
+interface MendatoryInterface {name: string, age: number, salary: number}
+
+
+const addMeInMyCrushMind =<T extends MendatoryInterface> (myInfo: T)=>{
+   const crush = 'Kate Winslet';
+   const newData = {...myInfo,crush}
+   return newData
+}
+
+type MyInfoType = {
+    name: string;
+    age: number;
+    salary:number
+}
+const myInfo : MyInfoType = {
+    name:'Sakkar',
+    age:100,
+    salary: 1000000
+}
+
+const result5 = addMeInMyCrushMind<MyInfoType>(myInfo)
--- src/generic-function.ts
+++ src/generic-function.ts
@@ -1,42 +1,42 @@
 
-// Array Function
+// // Array Function
 
-const createArray = (param:string): string[] => {
-return [param]
-}
+// const createArray = (param:string): string[] => {
+// return [param]
+// }
 
-//Generic for Arrow Function
-const createArray1 = <X,Y>(param1:X,param2:Y): [X,Y] => {
-return [param1,param2]
-}
+// //Generic for Arrow Function
+// const createArray1 = <X,Y>(param1:X,param2:Y): [X,Y] => {
+// return [param1,param2]
+// }
 
-//Generic for Normal Function
-function createArray2 <X,Y>(param1:X,param2:Y): [X,Y] {
-return [param1,param2]
-}
+// //Generic for Normal Function
+// function createArray2 <X,Y>(param1:X,param2:Y): [X,Y] {
+// return [param1,param2]
+// }
 
-const result1 = createArray1<string, string>('Bangladesh','I Love my Country')
-const result2 = createArray1<boolean, string[]>(true,['USA'])
+// const result1 = createArray1<string, string>('Bangladesh','I Love my Country')
+// const result2 = createArray1<boolean, string[]>(true,['USA'])
 
-type Name = {name:string}
-const result3 = createArray1<Name, boolean>({name: 'Bangladesh'}, true)
-
-
-//Spread Operator
-
-// const newData = {...myInfo,crush}
-
-const addMeInMyCrushMind =<T> (myInfo: T)=>{
-   const crush = 'Kate Winslet';
-   const newData = {...myInfo,crush}
-   return newData
-}
+// type Name = {name:string}
+// const result3 = createArray1<Name, boolean>({name: 'Bangladesh'}, true)
 
 
-const myInfo = {
-    name:'Sakkar',
-    age:100,
-    salary: 1000000
-}
+// //Spread Operator
 
-const result5 = addMeInMyCrushMind(myInfo)
+// // const newData = {...myInfo,crush}
+
+// const addMeInMyCrushMind =<T> (myInfo: T)=>{
+//    const crush = 'Kate Winslet';
+//    const newData = {...myInfo,crush}
+//    return newData
+// }
+
+
+// const myInfo = {
+//     name:'Sakkar',
+//     age:100,
+//     salary: 1000000
+// }
+
+// const result5 = addMeInMyCrushMind(myInfo)
