84e2255f7ce14bbe5e3bedefd0be9f4d3af6808c
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-06-18 18:14:32 +0600

    generic in Function

--- src/generic-interface.ts
+++ src/generic-interface.ts
@@ -1,25 +1,25 @@
-// Generic Interface
+// // Generic Interface
 
-interface CrushInterface<T,U = null>{
-    name:string,
-    husband:T,
-    wife?: U
-}
+// interface CrushInterface<T,U = null>{
+//     name:string,
+//     husband:T,
+//     wife?: U
+// }
 
-const crush1: CrushInterface<boolean, string> = {name:'Kate Winslet', husband: true, wife: 'Walfie'}
+// const crush1: CrushInterface<boolean, string> = {name:'Kate Winslet', husband: true, wife: 'Walfie'}
 
-const crush2: CrushInterface<string> = {name:'Kate Winslet', husband: 'Jack'}
+// const crush2: CrushInterface<string> = {name:'Kate Winslet', husband: 'Jack'}
 
-interface HusbandInterface {
-name:string,
- age:number
-}
+// interface HusbandInterface {
+// name:string,
+//  age:number
+// }
 
-const crush3: CrushInterface<HusbandInterface> = {name:'Kate Winslet', husband: {name:'Jack', age: 32}}
+// const crush3: CrushInterface<HusbandInterface> = {name:'Kate Winslet', husband: {name:'Jack', age: 32}}
 
-interface PersonInterface {
-    name:string, age: number
-}
+// interface PersonInterface {
+//     name:string, age: number
+// }
 
-const crush4: CrushInterface<PersonInterface, PersonInterface> = {name:'Kate Winslet', husband: {name:'Jack', age: 32}, wife:{name: 'Walfie', age: 25}}
+// const crush4: CrushInterface<PersonInterface, PersonInterface> = {name:'Kate Winslet', husband: {name:'Jack', age: 32}, wife:{name: 'Walfie', age: 25}}
 
