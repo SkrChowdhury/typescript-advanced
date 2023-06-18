5d9e011aea882bebd9502c9966f1dd3652a6b80d
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-06-18 12:07:34 +0600

    generic in interface

---
+++ src/generic-interface.ts
@@ -0,0 +1,25 @@
+// Generic Interface
+
+interface CrushInterface<T,U = null>{
+    name:string,
+    husband:T,
+    wife?: U
+}
+
+const crush1: CrushInterface<boolean, string> = {name:'Kate Winslet', husband: true, wife: 'Walfie'}
+
+const crush2: CrushInterface<string> = {name:'Kate Winslet', husband: 'Jack'}
+
+interface HusbandInterface {
+name:string,
+ age:number
+}
+
+const crush3: CrushInterface<HusbandInterface> = {name:'Kate Winslet', husband: {name:'Jack', age: 32}}
+
+interface PersonInterface {
+    name:string, age: number
+}
+
+const crush4: CrushInterface<PersonInterface, PersonInterface> = {name:'Kate Winslet', husband: {name:'Jack', age: 32}, wife:{name: 'Walfie', age: 25}}
+
--- src/generic-type.ts
+++ src/generic-type.ts
@@ -1,15 +1,23 @@
 
-type GenericTuple <X,Y> = [X,Y]
-const relation:GenericTuple<string,string> = ['Mr. X', 'Mrs. Y']
-
-const relationWithSalary: GenericTuple<object,string> =[{name: 'Mr. X', salary: 1000000},'Mrs. Y']
-
-type GenericArray<T> = Array<T>
-
-const rollNumbers1: GenericArray<number> = [1,2,3,4]
-const rollNumbers2: GenericArray<string> = ['one', 'two', 'three', 'four']
-const rollNumbers3: GenericArray<boolean> = [true, false]
+// type GenericTuple <X,Y> = [X,Y]
+// const relation:GenericTuple<string,string> = ['Mr. X', 'Mrs. Y']
 
 
-type NameRollType = {name:string, roll:number}
-const userNameAndRollNumbers: GenericArray<NameRollType> = [{name:'Mr. X', roll:1}, {name:'Mr. Y', roll:2}]
+// // type RelationWithSalaryType = {name: string, salary: number}
+// interface RelationWithSalaryInterface {name: string, salary: number}
+
+
+// // const relationWithSalary: GenericTuple<RelationWithSalaryType,string> =[{name: 'Mr. X', salary: 1000000},'Mrs. Y']
+
+// const relationWithSalary: GenericTuple<RelationWithSalaryInterface,string> =[{name: 'Mr. X', salary: 1000000},'Mrs. Y']
+
+
+// type GenericArray<T> = Array<T>
+
+// const rollNumbers1: GenericArray<number> = [1,2,3,4]
+// const rollNumbers2: GenericArray<string> = ['one', 'two', 'three', 'four']
+// const rollNumbers3: GenericArray<boolean> = [true, false]
+
+
+// type NameRollType = {name:string, roll:number}
+// const userNameAndRollNumbers: GenericArray<NameRollType> = [{name:'Mr. X', roll:1}, {name:'Mr. Y', roll:2}]
